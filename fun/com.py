1baf8f4fae1f44ae849ab7ead9d34d733e3a962c
Randal Savage <34065151+ytt447735@users.noreply.github.com>
2024-11-05 21:03:34 +0800

    Update com.py

--- fun/com.py
+++ fun/com.py
@@ -1,5 +1,15 @@
 import time
+import re
 
 # 获取时间戳
 def get_time():
     return int(round(time.time() * 1000))
+
+# 取中间文本
+def GetIntermediateText(text, start, end):
+    print(text)
+    pattern = re.escape(start) + '(.*?)' + re.escape(end)
+    match = re.search(pattern, text)
+    if match:
+        return match.group(1)
+    return ''
